8709f25428a54b1bbe9a40382a75853f78b8a51b
Brayan Benet <gabrielbrayan510@gmail.com>
2024-03-25 19:40:14 -0300

    feat: teste de login com dados inválidos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
+
 describe('Login', () => {
 
-    before(() => {
+    beforeEach(() => {
         cy.visit('')
     });
 
@@ -9,10 +10,23 @@ describe('Login', () => {
         cy.gerarDadosLogin()
         cy.readFile("cypress/fixtures/loginData.json").then((data) => {
             const usuarioValido = data.usuarioValido
-
+            
             cy.clicarBtnLogar()
             cy.preencherCampoEmail(usuarioValido.email)
             cy.preencherCampoSenha(usuarioValido.senha)
+            cy.clicarBtnLogin()
+            cy.clicarBtnLogout()
+        })
+    });
+
+    it('Teste 2 - Login com dados inválidos', () => {
+        cy.gerarDadosLogin
+        cy.readFile("cypress/fixtures/loginData.json").then((data) => {
+            const usuarioInvalido = data.usuarioInvalido
+
+            cy.clicarBtnLogar()
+            cy.preencherCampoEmail(usuarioInvalido.email)
+            cy.preencherCampoSenha(usuarioInvalido.senha)
             cy.clicarBtnLogin()
             cy.clicarBtnLogout()
         })
